3bef1267922612f26381e282f4e8968c41ae2e04
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-13 12:37:10 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
+int main(void) {
+    printf("char: %d\n", sizeof(char));
+    printf("shorts: %hd\n", sizeof(short));
+    printf("int: %d\n", sizeof(int));
+    printf("unsigned int: %u\n", sizeof(unsigned int));
+    printf("long: %ld\n", sizeof(long));
+    printf("long long: %ld\n", sizeof(long long));
+    printf("float: %d\n", sizeof(float));
 
     return 0;
 }
