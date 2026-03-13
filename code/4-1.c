e53da6218faa449685eb17737f000435e6b32a11
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-13 20:08:37 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int a, b;
+    char c;
+    scanf("%d %d %c", &a, &b, &c);
 
+    switch (c)
+    {
+    case '+':
+    printf("%d\n", a + b);
+        break;
+    case '-':
+    printf("%d\n", a - b);
+        break;
+    case '*':
+    printf("%d\n", a * b);
+        break;
+    case '/':
+    printf("%d\n", a / b);
+        break;   
+    }
     return 0;
 }
-
